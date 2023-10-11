f61fea79f7099d4249c4f6ce8a8d87a68428233a
Abel <camilosol123@gmail.com>
2023-10-11 01:51:03 -0500

    mucho sexo

--- app/Views/Dashboard/escritorio.php
+++ app/Views/Dashboard/escritorio.php
@@ -10,6 +10,7 @@
     {Examen}
 
 -->
+
 <head>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Crimson+Text|Open+Sans+Condensed:300|Raleway|Roboto" rel="stylesheet">
@@ -23,39 +24,55 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
 </head>
-<body style="width: 100%; color:#00fdf9;" class="cuerpa">
-    <section>
-        <br>
+
+<body style="Width:100% " class="cuerpa">
+    <section style="
+        width: 100%;">
+        <nav id='menu'>
+            <input type='checkbox' />
+            <label>&#8801;<span>Menu</span></label>
+            <ul>
+                <li><a href='/'>Inicio</a></li>
+                <li><a href='#'>Sobre mi</a></li>
+                <li><a href='#'>Categorías<font-size='1'>&#9660;</font-size></a>
+                    <ul class='menus'>
+                        <li><a href='#'>Categoría 1</a></li>
+                        <li><a href='#'>Categoría 2</a></li>
+                        <li><a href='#'>Categoría 3</a></li>
+                    </ul>
+                </li>
+                <li><a href='#'>Contacto</a></li>
+                <li style="margin-left: 5px; margin-right: 5px; margin-top:5px;"><a style="background-color: blue; width: 100px; height: 30px; border-radius: 10px; right: 15px;" href='#'>Acceder</a></li>
+            </ul>
+        </nav>
         <br><br>
         <br><br>
-        <div class='jumbotron' style="backdrop-filter: blur(10px);
-    background-color:rgba(250, 250, 250, 0.2);  text-align:center; Width:90% ">
-            <h3 class='display-4 relawayStandart text-light shadow-sm'>Calculadora de operaciones estadisticas
+        <br><br>
+
+        <div style="text-align:center; Width:100%">
+            <h3 class='display-4 relawayStandart text-light shadow-sm'>S T A T I S T I C A L - S T U D Y
             </h3>
+            <br><br>
+            <br><br>
         </div>
         </div>
-        <h5 class="instruc" style=" 
- text-align:center;
- font-size:11px;">Da click sobre las tarjetas, para ver su contenido</h5>
+        <h5 class="instruc" style=" text-align:center; font-size:15px; color:aqua">
+            Da click sobre las tarjetas, para ver su contenido</h5>
         <div class="container">
             <div class="card">
                 <div class="content">
-                    <h2>02</h2>
-                    <h3>Ejercicios</h3>
+                    <h2>01</h2>
+                    <h3>Ejercicios y actividades</h3>
                     <p>
-                        El método sirve para encontrar soluciones parciales completas, no soluciones generales,
-                        dependientes de un conjunto numerable de constantes arbitrarias, lo cual permite resolver
-                        tanto problemas de valor inicial como problemas de frontera.</p>
+                        Indica cuáles variables son cualitativas y cuáles cuantitativas.</p>
                     <a href="carga_ejercicios">Ver ejercicios</a>
                 </div>
             </div>
             <div class="card">
                 <div class="content">
-                    <h2>03</h2>
+                    <h2>02</h2>
                     <h3>Examen</h3>
-                    <p>Consideremos la ecuación y-y'=0, lo primero que debemos hacer es reescribir y' como un
-                        cociente de diferenciales \frac{dy}{dx}, de esta forma, reescribimos la ecuación de la
-                        siguiente maneras....</p>
+                    <p>¿Crees poder resolver este examen con eficacion y velosidad?.</p>
                     <a href="carga_examenes">Realizar examen</a>
                 </div>
             </div>
@@ -69,14 +86,20 @@
                 "max-glare": 1
             });
         </script>
+        <script src="js/desplegable.js" type="text/javascript"></script>
+        <script src="https://code.jquery.com/jquery-latest.js" type="text/javascript"></script>
+        <script src="js/show_search.js" type="text/javascript"></script>
+
+
 </body>
 <br><br>
 <br><br>
-<footer>
+<footer style="backdrop-filter: blur(10px);
+    background-color:rgba(250, 250, 250, 0.2);  text-align:center; ">
     <div class="container">
         <div class="row">
             <div class="col-sm-6 col-md-3 item">
-                <h3>Services</h3>
+                <h3 style="color:whitesmoke">Services</h3>
                 <ul>
                     <li><a target="_blank" href="https://www.instagram.com/camilo_abel1/">Web design</a></li>
                     <li><a target="_blank" href="#">Development</a></li>
@@ -84,7 +107,7 @@
                 </ul>
             </div>
             <div class="col-sm-6 col-md-3 item">
-                <h3>About</h3>
+                <h3 style="color:whitesmoke">About</h3>
                 <ul>
                     <li><a href="#">Company</a></li>
                     <li><a href="#">Team</a></li>
@@ -92,8 +115,8 @@
                 </ul>
             </div>
             <div class="col-md-6 item text">
-                <h3>STATISTICAL STUDY</h3>
-                <p>Somos tu mejor obcion si lo que quieres es fortalecer los conocimientos frente a las estadisticas,
+                <h3 style="color:whitesmoke">STATISTICAL STUDY</h3>
+                <p style="color:aqua">Somos tu mejor obcion si lo que quieres es fortalecer los conocimientos frente a las estadisticas,
                     las estadisticas son un campo amplio
                     en el cual puedes aprender muchas cosas que pueden ofreserte
                     gran fortaleza en e sector empleo.</p>
@@ -115,6 +138,32 @@
         to {
             width: 100%
         }
+    }
+
+
+    .logo {
+        left: 15px;
+        transform: translatex(-342px) translatey(-4px);
+    }
+
+    .menu {
+        list-style: none;
+        padding: 0;
+        display: flex;
+    }
+
+    .menu li {
+        margin-right: 20px;
+    }
+
+    .menu a {
+        text-decoration: none;
+        color: #fff;
+        font-weight: bold;
+    }
+
+    .menu a:hover {
+        border-bottom: 2px solid #fff;
     }
 
     .bar {
@@ -194,6 +243,7 @@
         margin: 0;
         padding: 0
     }
+
     #primary_nav_wrap ul a {
 
         display: block;
@@ -205,6 +255,7 @@
         padding: 0 15px;
         font-family: "HelveticaNeue", "Helvetica Neue", Helvetica, Arial, sans-serif
     }
+
     #primary_nav_wrap ul li {
         border-radius: 10px;
         position: relative;
@@ -212,6 +263,7 @@
         margin: 0;
         padding: 0
     }
+
     #primary_nav_wrap ul ul {
         display: none;
         position: absolute;
@@ -257,11 +309,6 @@
     body::-webkit-scrollbar {
         width: 10px;
         /* width of the entire scrollbar */
-    }
-
-    body::-webkit-scrollbar-track {
-        background: aqua;
-        /* color de fondo pagina principal */
     }
 
     body::-webkit-scrollbar-thumb {
@@ -376,6 +423,222 @@
         text-decoration: none;
         font-weight: 500;
         box-shadow: 0 5px 15px rgba(0, 0, 0, 0.2);
+    }
 
+    /* Column 6/12 */
+    section div .item {
+        padding-top: 50px;
+    }
+
+    /* Paragraph */
+    .row .bar p {
+        color: #ffffff;
+    }
+
+    /* Paragraph */
+    .row .item p {
+        text-align: justify;
+    }
+
+    /*-----Menu responsive con subcategorias de MBT modificado por Minerva Aurora----*/
+    body {
+        
+        width: 100%;
+        padding-right: 0px;
+        padding-left: 0px;
+    }
+
+    #menu {
+        height: 40px;
+        width: 100%;
+        position: fixed;
+        backdrop-filter: blur(10px);
+        background-color: rgba(250, 250, 250, 0.2);
+        z-index: 101;
+    }
+
+    #menu ul,
+    #menu li {
+        margin: 0 auto;
+        padding: 0 0;
+        list-style: none
+    }
+
+    #menu ul {
+        height: 45px;
+        width: 1000px;
+        /* ancho del menú, ajustar para centrarlo*/
+    }
+
+    #menu li {
+        float: left;
+        display: inline;
+        position: relative;
+        font: 11px Arial, sans-serif;
+        /*tamaño y fuente del texto */
+    }
+
+    #menu a {
+        display: block;
+        line-height: 40px;
+        padding: 0 14px;
+        text-decoration: uppercase;
+        color: white;
+        /*color del texto*/
+        text-transform: uppercase;
+        /*letras mayúsculas*/
+        letter-spacing: 1.5px;
+        /*espacio entre letras*/
+    }
+
+    #menu li a:hover {
+        color: blueviolet;
+        /*color del texto al pasar el ratón*/
+        text-decoration: none;
+        -webkit-transition: all .1s ease-in-out;
+        -moz-transition: all .1s ease-in-out;
+        -ms-transition: all .1s ease-in-out;
+        -o-transition: all .1s ease-in-out;
+        transition: all .1s ease-in-out;
+        background: #E9E9E9;
+        /*color del fondo de la categoria al pasar el ratón*/
+    }
+
+    #menu input {
+        display: none;
+        margin: 0 0;
+        padding: 0 0;
+        width: 80px;
+        height: 30px;
+        opacity: 0;
+        cursor: pointer
+    }
+
+    #menu label {
+        font: 25px Arial, sans-serif;
+        font-weight: bold;
+        display: none;
+        width: 35px;
+        height: 35px;
+        line-height: 43px;
+        text-align: center;
+    }
+
+    #menu label span {
+        font: 12px Arial, sans-serif;
+        /*tamaño y fuente del texto "menu" */
+        position: absolute;
+        left: 35px;
+        text-transform: uppercase;
+        font-weight: bold;
+        line-height: 40px;
+        letter-spacing: 0.5px;
+    }
+
+    #menu ul.menus {
+        height: auto;
+        overflow: hidden;
+        width: 170px;
+        background: #F1F1F1;
+        /*color del fondo del submenu*/
+        position: absolute;
+        z-index: 99;
+        display: none;
+        border: 1px solid #E9E9E9;
+    }
+
+    #menu ul.menus li {
+        display: block;
+        width: 100%;
+        font: 11px Arial, sans-serif;
+        /*tamaño y fuente del  texto del submenu*/
+        text-transform: uppercase;
+        border-bottom: 1px solid #E9E9E9;
+    }
+
+    #menu ul.menus li:last-child {
+        border-bottom: none;
+    }
+
+    #menu ul.menus a {
+        color: #333;
+        /*color del  texto del submenu*/
+        line-height: 35px;
+    }
+
+    #menu li:hover ul.menus {
+        display: block
+    }
+
+    #menu ul.menus a:hover {
+        background: #E9E9E9;
+        /*color del  fondo del submenu al pasar el ratón*/
+        color: #000;
+        /*color del  texto del submenu al pasar el ratón*/
+        -webkit-transition: all .1s ease-in-out;
+        -moz-transition: all .1s ease-in-out;
+        -ms-transition: all .1s ease-in-out;
+        -o-transition: all .1s ease-in-out;
+        transition: all .1s ease-in-out;
+    }
+
+    @media screen and (max-width: 800px) {
+        #menu {
+            position: fixed;
+            z-index: 101;
+        }
+
+        #menu ul {
+            background: #F1F1F1;
+            /*color del desplegable del menu movil*/
+            position: absolute;
+            top: 100%;
+            right: 0;
+            left: 0;
+            z-index: 3;
+            height: auto;
+            display: none
+        }
+
+        #menu ul.menus {
+            width: 100%;
+            position: static;
+            padding-left: 20px
+        }
+
+        #menu li {
+            display: block;
+            float: none;
+            width: auto;
+            font: 11px Arial, sans-serif;
+        }
+
+        #menu a {
+            display: block;
+            line-height: 40px;
+            padding: 0 14px;
+            /*tamaño y fuente del  menu movil*/
+        }
+
+        #menu input,
+        #menu label {
+            position: absolute;
+            top: 0;
+            left: 0;
+            display: block
+        }
+
+        #menu input {
+            z-index: 4
+        }
+
+        #menu input:checked+label {
+            color: #333;
+            /*color de las tres barritas una vez se clica*/
+        }
+
+        #menu input:checked~ul {
+            display: block
+        }
     }
 </style>
